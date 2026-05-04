e76587d0918a5de2883901e67d1304ecf8a0c38e
Serghei Granici <sergheigranici@Mac-mini-Serghei.local>
2026-05-04 22:04:47 +0200

    HW_03 is done

---
+++ Nodejs_HW_03/index.js
@@ -0,0 +1,17 @@
+import fs from "fs";
+
+fs.mkdir("myFolder", (err) => {
+  if (err) {
+    console.log("Error: Folder is not created");
+  } else {
+    console.log("Folder is created successfully!");
+  }
+
+  fs.rmdir("myFolder", (err) => {
+    if (err) {
+      console.log("Error: Folder is not deleted");
+    } else {
+      console.log("Folder is deleted successfully!");
+    }
+  });
+});
